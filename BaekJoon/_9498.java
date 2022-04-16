9c742b681474e89b94c606f9e0f4c96927e2d739
Yoonmen <sgu101310@naver.com>
2022-04-17 01:16:32 +0900

    시험 성적

---
+++ BaekJoon/_9498.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class _9498 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int score;
+        score = sc.nextInt();
+        sc.close();
+        
+        if (90 <= score) {
+            System.out.println("A");
+        }
+        else if (80 <= score) {
+            System.out.println("B");
+        }
+        else if (70 <= score) {
+            System.out.println("C");
+        }
+        else if (60 <= score) {
+            System.out.println("D");
+        }
+        else {
+            System.out.println("F");
+        }
+    }
+}
