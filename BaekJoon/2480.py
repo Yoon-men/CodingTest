1fabd991fd3debde8fb280fc70ae62abba8bcbc8
Yoonmen <sgu101310@naver.com>
2022-04-18 01:01:12 +0900

    주사위 세 개

---
+++ BaekJoon/2480.py
@@ -0,0 +1,9 @@
+# 주사위 세 개
+dice = list(map(int, input().split()))
+dice.sort()
+if dice[0] == dice[1] == dice[2] : 
+    print(10000 + dice[0]*1000)
+elif dice[0] == dice[1] or dice[1] == dice[2] : 
+    print(1000 + dice[1]*100)
+else : 
+    print(max(dice) * 100)
