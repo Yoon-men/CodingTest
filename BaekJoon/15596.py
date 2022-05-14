1da5b6247a3a02b0a0b69a4a1dbbcd6fbf3a927c
Yoonmen <sgu101310@naver.com>
2022-05-14 16:11:35 +0900

    정수 N개의 합

---
+++ BaekJoon/15596.py
@@ -0,0 +1,6 @@
+# 정수 N개의 합
+def solve(a) : 
+    return(sum(a))
+
+# a = [1, 2, 3]               # Test code / please lock the contents of this line for submission.
+# print(homeMinus(a))         # Test code / please lock the contents of this line for submission.
