8f082180d53861655e5418c7fdd171dd58fa4622
Yoonmen <sgu101310@naver.com>
2022-05-24 01:42:08 +0900

    이항 계수 1

---
+++ BaekJoon/11050.py
@@ -0,0 +1,11 @@
+# 이항 계수 1
+import sys
+input = sys.stdin.readline
+N, K = map(int, input().split())
+
+def factorial(n) : 
+    if n == 0 : 
+        return 1
+    return n * factorial(n - 1)
+
+print(factorial(N) // (factorial(K)*factorial(N - K)))
