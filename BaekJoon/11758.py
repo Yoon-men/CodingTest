e71f91a9da386cfb3b6a4f5c54eca3eb02ff527a
Yoonmen <sgu101310@naver.com>
2022-07-03 00:07:09 +0900

    CCW

--- BaekJoon/11758.py
+++ BaekJoon/11758.py
@@ -5,7 +5,7 @@ def ccw(p1, p2, p3) :
     x1, y1 = p1
     x2, y2 = p2
     x3, y3 = p3
-    return (x1*y2 + x2*y3 + x3*y1) - (x2*y1 + x3*y2 + x1*y3)
+    return (x2-x1)*(y3-y1) - (x3-x1)*(y2-y1)
 
 if __name__ == "__main__" : 
     dots = [list(map(int, sys.stdin.readline().split())) for _ in range(3)]
