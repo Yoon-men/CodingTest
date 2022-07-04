08fb29c718272a5d6467738e87dfc8613ee46e00
Yoonmen <sgu101310@naver.com>
2022-07-04 17:53:15 +0900

    숫자의 개수

--- BaekJoon/2577.py
+++ BaekJoon/2577.py
@@ -1,19 +1,11 @@
-# 숫자의 개수
+# 백준2577 : 숫자의 개수
+import sys
+input = sys.stdin.readline
 
-A = int(input(""))
-B = int(input(""))
-C = int(input(""))
+A = int(input())
+B = int(input())
+C = int(input())
 
-ABC = A * B * C
-
-ABC_List = list(str(ABC))
-ABC_appearNum = [0] * 10
-
+ABC = list(str(A * B * C))
 for i in range(10) : 
-    if str(i) in ABC_List : 
-        for j in range(len(ABC_List)) : 
-            if ABC_List[j] == str(i) : 
-                ABC_appearNum[i] += 1
-
-for i in ABC_appearNum : 
-    print(i)
+    print(ABC.count(str(i)))
