4610369d55e05e2ff3de814fe0646225f2517445
Yoonmen <sgu101310@naver.com>
2022-07-11 15:06:48 +0900

    최소공배수

---
+++ BaekJoon/1934.py
@@ -0,0 +1,14 @@
+# 백준1934 : 최소공배수
+import sys
+input = sys.stdin.readline
+
+for _ in range(int(input())) : 
+    A, B = map(int, input().split())
+    a, b = A, B
+    while b != 0 : 
+        a %= b
+        a, b = b, a
+    GCD = a             # Great Common Divisor(GCD)
+    LCM = A*B // GCD    # Largest Common Multiple(LCM)
+    
+    print(LCM)
