222dc91d062ecd49cb74d43e0cace9dbe3e6a303
Yoonmen <sgu101310@naver.com>
2022-09-22 22:29:10 +0900

    보물섬

--- BaekJoon/2589.py
+++ BaekJoon/2589.py
@@ -12,25 +12,21 @@ def joyGo(i, j) :
         x, y = dq.popleft()
         for k in range(4) : 
             nx = x + dx[k]
-            ny = y = dy[k]
-            if (0 <= nx < H) and (0 <= ny < V) and (not visited[nx][ny]) and (m[nx][ny] == "L") : 
+            ny = y + dy[k]
+            if (0 <= nx < V) and (0 <= ny < H) and (not visited[nx][ny]) and (m[nx][ny]=="L") : 
                 visited[nx][ny] = visited[x][y] + 1
                 cnt = max(cnt, visited[nx][ny])
                 dq.append([nx, ny])
-    print(visited)              # Test code / please delete the contents of this line.
-    return cnt
+    return cnt-1
 
 if __name__ == "__main__" : 
-    # V, H = map(int, input().split())              # Test code / please unlock the contents of this line.
-    V, H = 5, 7             # Test code / please delete the contents of this line.
-    # m = [input().strip() for _ in range(V)]               # Test code / please unlock the contents of this lien.
-    m = "WLLWWWL LLLWLLL LWLWLWW LWLWLLL WLLWLWW".split()             # Test code / please delete the contents of this line.
+    V, H = map(int, input().split())
+    m = [input().strip() for _ in range(V)]
     dx = [1, -1, 0, 0]
     dy = [0, 0, 1, -1]
 
     answer = 0
     for i in range(V) : 
         for j in range(H) : 
-            if m[i][j]=="L" : 
-                answer = max(answer, joyGo(i, j))
+            if m[i][j]=="L" : answer = max(answer, joyGo(i, j))
     print(answer)
