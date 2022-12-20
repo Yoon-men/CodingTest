b94cb109728c641c1f84f4e4a3e72cc25ba033f2
Yoonmen <sgu101310@naver.com>
2022-12-21 01:01:35 +0900

    안녕

--- BaekJoon/1535.py
+++ BaekJoon/1535.py
@@ -1,16 +1,18 @@
 # 백준1535 : 안녕
-N = int(input())
-dLi = [0] + list(map(int, input().split()))     # demage list
-hLi = [0] + list(map(int, input().split()))     # happiness list
+def chk(hp, dLi, hLi, N) : 
+    dp = [[0]*(hp+1) for _ in range(N+1)]
+    for i in range(1, N+1) : 
+        for j in range(1, hp+1) : 
+            if dLi[i-1] <= j : 
+                dp[i][j] = max(hLi[i-1] + dp[i-1][j-dLi[i-1]], dp[i-1][j])
+            else : 
+                dp[i][j] = dp[i-1][j]
+    return dp[N][hp-1]
 
-hp = 100
+if __name__ == "__main__" : 
+    N = int(input())
+    dLi = list(map(int, input().split()))     # demage list
+    hLi = list(map(int, input().split()))     # happiness list
+    hp = 100
 
-dp = [[0]*(hp+1) for _ in range(N+1)]
-for i in range(1, N+1) : 
-    for j in range(1, hp+1) : 
-        if dLi[i] <= j : 
-            dp[i][j] = max(hLi[i-1] + dp[i-1][j-dLi[i]], dp[i-1][j])
-        else : 
-            dp[i][j] = dp[i-1][j]
-
-print(dp[N][hp-1])
+    print(chk(hp, dLi, hLi, N))
