9cf2306e7ece278015d554bab79b50e97fb16bf9
Yoonmen <yoonmen03@gmail.com>
2023-01-30 15:57:08 +0900

    가장 긴 감소하는 부분 수열

---
+++ BaekJoon/11722.py
@@ -0,0 +1,15 @@
+# 백준11722 : 가장 긴 감소하는 부분 수열
+def joyGo() : 
+    dp = [0] * N
+    for i in range(N) : 
+        for j in range(N) : 
+            if (Li[i] < Li[j]) and (dp[i] < dp[j]) : 
+                dp[i] = dp[j]
+        dp[i] += 1
+
+    return max(dp)
+
+if __name__ == "__main__" : 
+    N = int(input())
+    Li = list(map(int, input().split()))
+    print(joyGo())
