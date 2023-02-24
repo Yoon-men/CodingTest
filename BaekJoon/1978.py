85d9a465764f16d75b8b868f68dcf4135a67d743
Yoonmen <yoonmen03@gmail.com>
2023-02-24 13:34:56 +0900

    소수 찾기

--- BaekJoon/1978.py
+++ BaekJoon/1978.py
@@ -1,20 +1,19 @@
-# 소수 찾기
-N = int(input())
-figure = list(map(int, input().split()))
-count = 0
+# 백준1978 : 소수 찾기
+def isPrime(n) : 
+    if n == 1 : return False
 
-for i in figure : 
-    prime = True
+    for i in range(2, int(n**0.5)+1) : 
+        if n % i == 0 : return False
 
-    if i == 1 : 
-        continue
+    return True
 
-    for j in range(2, i) : 
-        if i % j == 0 : 
-            prime = False
-            break
 
-    if prime == True : 
-        count += 1
+if __name__ == "__main__" : 
+    _ = int(input())
+    Li = list(map(int, input().split()))
 
-print(count)
+    ans = 0
+    for i in Li : 
+        if isPrime(i) : ans += 1
+
+    print(ans)
