4ac34deee5da3736f752d7aa4e2b664af0f88770
Yoonmen <yoonmen03@gmail.com>
2023-03-05 12:32:44 +0900

    녹색 옷 입은 애가 젤다지?

--- BaekJoon/4485.py
+++ BaekJoon/4485.py
@@ -1,34 +1,35 @@
 # 백준4485 : 녹색 옷 입은 애가 젤다지?
-from collections import deque
-import sys ; input = sys.stdin.readline
+import sys; input = sys.stdin.readline
+from heapq import heappush, heappop
 
-def joyGo(i, j) : 
-    dq = deque()
-    dq.append((i, j))
-    visited = [[0]*N for _ in range(N)]
-    visited[i][j] = 1
+def dijkstraJoyGo(graph) : 
+    N = len(graph)
+    dx, dy = [-1,1,0,0], [0,0,1,-1]
+
+    shitLi = [[sys.maxsize]*N for _ in range(N)]; shitLi[0][0] = 0
+    que = [(graph[0][0], 0, 0)]
     
-    while dq : 
-        x, y = dq.popleft()
-        for k in range(4) : 
-            nx = x + dx[k]
-            ny = y + dy[k]
-            if (0 <= nx < N) and (0 <= ny < N) and (not visited[nx][ny]) : 
-                if chkLi[nx][ny] > chkLi[x][y] + m[nx][ny] : 
-                    chkLi[nx][ny] = chkLi[x][y] + m[nx][ny]
-                    dq.append((nx, ny))
+    while que : 
+        shit, x, y = heappop(que)
+        if (x == N-1) and (y == N-1) : return shitLi[x][y]
+
+        for i in range(4) : 
+            nx, ny = x+dx[i], y+dy[i]
+            if (0 <= nx < N) and (0 <= ny < N) : 
+                total_shit = shit + graph[nx][ny]
+                if total_shit < shitLi[nx][ny] : 
+                    shitLi[nx][ny] = total_shit
+                    heappush(que, (total_shit, nx, ny))
+
 
 if __name__ == "__main__" : 
-    stage = 1
+    cnt = 1
+
     while True : 
         N = int(input())
-        if N==0 : break
-        m = [list(map(int, input().split())) for _ in range(N)]
-        dx = [1, -1, 0, 0]
-        dy = [0, 0, 1, -1]
-        chkLi = [[sys.maxsize]*N for _ in range(N)]
-        chkLi[0][0] = m[0][0]
+        if N == 0 : break
 
-        joyGo(0, 0)
-        print(f"Problem {stage}: {chkLi[N-1][N-1]}")
-        stage += 1
+        graph = [list(map(int, input().split())) for _ in range(N)]
+
+        print(f"Problem {cnt}: {dijkstraJoyGo(graph)}")
+        cnt += 1
