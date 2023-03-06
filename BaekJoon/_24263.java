4679c44c2d1d21aaba44bf084d75b6f59315a344
Yoonmen <yoonmen03@gmail.com>
2023-03-06 22:40:38 +0900

    알고리즘 수업 - 알고리즘의 수행 시간 2

---
+++ BaekJoon/_24263.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class _24263 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        int n;
+        n = scanner.nextInt();
+
+        System.out.println(n);
+        System.out.println(1);
+
+        scanner.close();
+    }
+}
