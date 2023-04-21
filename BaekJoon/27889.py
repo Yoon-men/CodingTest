609dbbd276f110b374a27c55b9feef85b0bb4e53
Yoonmen <yoonmen03@gmail.com>
2023-04-21 23:44:37 +0900

    특별한 학교 이름

---
+++ BaekJoon/27889.py
@@ -0,0 +1,3 @@
+# 백준27889 : 특별한 학교 이름
+Di = {"NLCS": "North London Collegiate School", "BHA": "Branksome Hall Asia", "KIS": "Korea International School", "SJA": "St. Johnsbury Academy"}
+print(Di[input()])
