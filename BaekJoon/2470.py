165734d26a5fb450b8fa3274d6b1d2a3689f67c0
Yoonmen <yoonmen03@gmail.com>
2023-05-08 21:27:20 +0900

    두 용액

---
+++ BaekJoon/2470.py
@@ -0,0 +1,25 @@
+# 백준2470 : 두 용액
+import sys; input = sys.stdin.readline
+
+def joyGo(N: int, Li: list) -> list : 
+    Li.sort()
+    s, e = 0, N-1
+    total = sys.maxsize
+    ans_list = []
+
+    while s < e : 
+        a, b = Li[s], Li[e]
+        tmp = a + b
+        if abs(tmp) < total : 
+            total = abs(tmp)
+            ans_list = [a, b]
+        if tmp < 0 : s += 1
+        else : e -= 1
+
+    return ans_list
+
+
+if __name__ == "__main__" : 
+    N = int(input())
+    Li = list(map(int, input().split()))
+    print(*joyGo(N, Li))
