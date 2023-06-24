84266bc3935085abce8876c438f164397919b6cf
Yoonmen <yoonmen03@gmail.com>
2023-06-25 00:31:42 +0900

    평균은 넘겠지

--- BaekJoon/4344.py
+++ BaekJoon/4344.py
@@ -1,13 +1,21 @@
-# 평균은 넘겠지
-import sys
-input = sys.stdin.readline
-C = int(input())
-for _ in range(C) : 
-    scores = list(map(int, input().split()))
-    average = sum(scores[1:])/scores[0]
-    goodBoy = 0
-    for i in scores[1:] : 
-        if i > average : 
-            goodBoy += 1
-    percentage = goodBoy/scores[0]*100
-    print(f"{percentage:.3f}%")
+# 백준4344 : 평균은 넘겠지
+import sys; input = sys.stdin.readline
+
+def joyGo(N: int, Li: list) -> float : 
+    average = sum(Li) / N
+    cnt = 0
+    for s in Li : 
+        if s > average : cnt += 1
+
+    def fuckfuckfuckfuckfuckfuckfuckfuck(n: float, asshole: int) : 
+        shit = 10**asshole
+        n *= shit
+        res = int(n)+1 if (n-int(n) >= 0.5) else int(n)
+        return res / shit
+    return f"{fuckfuckfuckfuckfuckfuckfuckfuck(cnt/N * 100, 3):.3f}"
+
+
+if __name__ == "__main__" : 
+    for _ in range(int(input())) : 
+        N, *Li = map(int, input().split())
+        print(f"{joyGo(N, Li)}%")
