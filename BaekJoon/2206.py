228c437d74c5b51a71b2040fd9aabae6ee73625b
Yoonmen <yoonmen03@gmail.com>
2023-08-18 18:29:14 +0900

    벽 부수고 이동하기

---
+++ BaekJoon/2206.py
@@ -0,0 +1,41 @@
+# 백준2206 : 벽 부수고 이동하기
+import sys; input = sys.stdin.readline
+from typing import List
+from collections import deque
+
+def joyGo(N: int, M: int, _map: List[List[int]]) -> int : 
+    def BFS() -> int : 
+        dx, dy = [-1,1,0,0], [0,0,1,-1]
+        visited = [[[0] * 2 for _ in range(M)] for _ in range(N)]
+
+        dq = deque([(0, 0, 1)])
+        while dq : 
+            x, y, z = dq.popleft()
+
+            if (x == N-1) and (y == M-1) : return visited[x][y][z] + 1
+            
+            if z > 0 : 
+                for i in range(4) : 
+                    nx, ny = x+dx[i], y+dy[i]
+                    if (0 <= nx < N) and (0 <= ny < M) and (not visited[nx][ny][z-1]) : 
+                        dq.append((nx, ny, z-1))
+                        visited[nx][ny][z-1] = visited[x][y][z] + 1
+            
+            for i in range(4) : 
+                nx, ny = x+dx[i], y+dy[i]
+                if (0 <= nx < N) and (0 <= ny < M) and (_map[nx][ny] != 1) and (not visited[nx][ny][z]) : 
+                    dq.append((nx, ny, z))
+                    visited[nx][ny][z] = visited[x][y][z] + 1
+        
+        return -1
+
+
+    return BFS()
+
+
+
+if __name__ == "__main__" : 
+    N, M = map(int, input().split())
+    _map = [list(map(int, input().strip())) for _ in range(N)]
+
+    print(joyGo(N, M, _map))
