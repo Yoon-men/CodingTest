3cc9412b3f9478cc50cc95dbd9bcf12f5ee8c57b
Yoonmen <yoonmen03@gmail.com>
2023-09-06 02:29:46 +0900

    최소비용 구하기

--- BaekJoon/1916.py
+++ BaekJoon/1916.py
@@ -1,32 +1,36 @@
 # 백준1916 : 최소비용 구하기
+import sys; input = sys.stdin.readline
+from typing import Tuple
 from heapq import heappush, heappop
-import sys ; input = sys.stdin.readline
 
-def joyGo(startJoyGo) : 
+def joyGo(N: int, bus_infos: Tuple[Tuple[int, int, int]], departure: int, arrival: int) -> int : 
+    bus_graph = [[] for _ in range(N+1)]
+    for current_departure, current_arrival, current_charge in bus_infos : 
+        bus_graph[current_departure].append((current_arrival, current_charge))
+
+    charge_list = [sys.maxsize] * (N+1)
+
     hq = []
-    heappush(hq, (0, startJoyGo))
-    visited[startJoyGo] = 0
+    heappush(hq, (0, departure))
+    charge_list[departure] = 0
 
     while hq : 
-        p, d = heappop(hq)      # pay, departure
-        if visited[d] < p : continue
+        current_charge, current_departure = heappop(hq)
+        if charge_list[current_departure] < current_charge : continue
 
-        for endJoyGo, pay in graph[d] : 
-            totalPay = p + pay
+        for next_arrival, next_charge in bus_graph[current_departure] : 
+            total_charge = current_charge + next_charge
 
-            if visited[endJoyGo] > totalPay : 
-                heappush(hq, (totalPay, endJoyGo))
-                visited[endJoyGo] = totalPay
+            if charge_list[next_arrival] > total_charge : 
+                heappush(hq, (total_charge, next_arrival))
+                charge_list[next_arrival] = total_charge
+
+    return charge_list[arrival]
+
 
 if __name__ == "__main__" : 
     N = int(input())
-    visited = [sys.maxsize] * (N+1)
-
-    graph = [[] for _ in range(N+1)]
-    for _ in range(int(input())) : 
-        s, e, p = map(int, input().split())     # start, end, pay
-        graph[s].append((e, p))
+    bus_infos = tuple(tuple(map(int, input().split())) for _ in range(int(input())))
     departure, arrival = map(int, input().split())
 
-    joyGo(departure)
-    print(visited[arrival])
+    print(joyGo(N, bus_infos, departure, arrival))
