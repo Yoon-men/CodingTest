0ba956c68be8286814613d277a8ab5d419586b90
Yoonmen <yoonmen03@gmail.com>
2023-09-13 16:55:06 +0900

    수학숙제

---
+++ BaekJoon/2870.py
@@ -0,0 +1,16 @@
+# 백준2870 : 수학숙제
+import sys; input = sys.stdin.readline
+from typing import Tuple, List
+from re import split as reSplit
+
+def joyGo(txts: Tuple[str]) -> List[int] : 
+    ans_list = []
+    for txt in txts : ans_list.extend(reSplit("[a-z]", txt))
+    
+    ans_list = sorted([int(item) for item in ans_list if item])
+    return ans_list
+4
+
+if __name__ == "__main__" : 
+    txts = tuple(input().strip() for _ in range(int(input())))
+    print(*joyGo(txts), sep='\n')
