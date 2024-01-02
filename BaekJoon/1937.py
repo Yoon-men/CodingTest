c44cec431c3591fe82aa212ae6fc0ff143929f6b
Yoonmen <sgu101310@naver.com>
2024-01-02 22:42:49 +0900

    욕심쟁이 판다

--- BaekJoon/1937.py
+++ BaekJoon/1937.py
@@ -9,14 +9,17 @@ def joyGo(n: int, graph: List[List[int]]) -> int:
             return visited[x][y]
 
         visited[x][y] = 1
+
         for i in range(4): 
             nx, ny = x+dx[i], y+dy[i]
+
             if (0 <= nx < n) and (0 <= ny < n) and (graph[nx][ny] > graph[x][y]): 
                 visited[x][y] = max(visited[x][y], dfs(nx, ny) + 1)
 
         return visited[x][y]
 
         ### ----- dfs() end ----- ###
+
 
     dx, dy = [-1,1,0,0], [0,0,1,-1]
     visited = [[-1] * n for _ in range(n)]
@@ -25,7 +28,7 @@ def joyGo(n: int, graph: List[List[int]]) -> int:
         for j in range(n): 
             if visited[i][j] == -1: 
                 dfs(i, j)
-    
+
     ans = 0
     for i in range(n): 
         ans = max(ans, max(visited[i]))
@@ -33,7 +36,6 @@ def joyGo(n: int, graph: List[List[int]]) -> int:
     return ans
 
     ### ----- joyGo() end ----- ###
-
 
 
 if __name__ == "__main__": 
